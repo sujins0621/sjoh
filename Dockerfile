6ca608b2e1888585e67f1927018ba61e28942f7f
sjoh <sujins0621@gmail.com>
2024-05-21 23:22:13 +0900

    securityConfig

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM openjdk:17-oracle
-ADD ./build/libs/*.jar sjoh-0.0.1-SNAPSHOT.jar
+ADD ./build/libs/*.jar app.jar
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/server/sjoh/home/controller/HomeController.java
+++ src/main/java/com/server/sjoh/home/controller/HomeController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/homes")
 public class HomeController {
 
     @GetMapping("/home")
